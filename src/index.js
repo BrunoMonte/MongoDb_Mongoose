e9dcd077e80f011ae0e2e0e26a31c273fee2cbaf
Bruno Monte <brunoaguiarprofissional@gmail.com>
2022-01-17 21:26:56 -0300

    inicial

--- src/index.js
+++ src/index.js
@@ -3,13 +3,37 @@ const mongoose =  require('mongoose')
 const port = 3000
 const app = express()
 
-mongoose.connect('mongodb://localhost/links').then(db => {
-    console.log(db)
-}).catch(error => {
-    console.log(error)
-}) 
+const linkSchema = new mongoose.Schema({
+    title: String,
+    description:String,
+    url:String,
+    click:Number
+})
 
-app.get('/', (req,res) => res.send('chegou !!'))
+const Link = mongoose.model('Link', linkSchema)
+
+let link = new Link({
+    title:"Programdor",
+    description:"Link para Linkedin",
+    url:"https://www.linkedin.com/in/bruno-aguiar-01b5481aa/",
+    click : 0
+})
+
+link.save().then(doc => {
+    console.log(doc)
+}).catch(err =>{console.log(err)})
+
+mongoose.connect('mongodb://localhost/links', {
+    useNewUrlParser: true,
+    userUnifiedTopology: true
+})
+
+let db = mongoose.connection
+
+db.on("error", () => { console.log('Houve um erro') })
+db.once("open", () => { console.log('Banco Carregado') })
+
+
 
 app.listen(port, () => {
     console.log(`Api está rodando na porta http:/localhost:${port}`)
